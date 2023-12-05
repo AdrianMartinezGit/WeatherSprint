de5fccb0777e59d866f27efc808ac1d3d1dfd1f1
AdrianMartinezGit <adrionmtz02@gmail.com>
2023-12-05 11:53:17 -0800

    Add Searchbar and Autocomplete Function, and add Google Font

---
+++ scripts/autocomplete.js
@@ -0,0 +1,41 @@
+let availableKeyWords = [
+    "Antioch",
+    "Airport",
+    "Austin",
+    "Avenue"
+];
+
+const resultsBox = document.querySelector('.result-box');
+const inputBox   = document.getElementById('input-box');
+
+inputBox.onkeyup = function() {
+    let result = [];
+    let input = inputBox.value;
+
+    if (input.length) {
+        result = availableKeyWords.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+
+        console.log(result);
+    }
+
+    display(result);
+
+    if (!result.length) {
+        resultsBox.innerHTML = '';
+    }
+}
+
+function display(result) {
+    const content = result.map((list) => {
+        return "<li onclick=SelectInput(this)>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>"
+}
+
+function SelectInput(list) {
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = "";
+}
